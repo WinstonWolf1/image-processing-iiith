9cc297e85aa04fcc3a78a95249d70792e8b766f6
Rohan Nawathe <rohannawathe@hotmail.com>
2019-07-31 13:11:24 +0530

    Added scaling transform

--- SRIP/Codes/affine.js
+++ SRIP/Codes/affine.js
@@ -1,6 +1,6 @@
 /**
  *  Returns a image translated by xOffset in x direction and yOffset in y direction
- * 
+ *
  * @param {ImageData} inputImage
  * @param {number} xOffset
  * @param {number} yOffset
@@ -31,11 +31,12 @@ function translate(inputImage, xOffset, yOffset){
  *
  * @param {ImageData} inputImage
  * @param {number} angle
+ * @param {string} interpolType
  */
-function rotate(inputImage, angle, interpolType){
+function rotate(inputImage, angle, interpolType) {
     let outputImage = new ImageData(inputImage.width, inputImage.height);
 
-    for(let i = 0; i < outputImage.data.length; i += 4){
+    for(let i = 0; i < outputImage.data.length; i += 4) {
         // Coordinates from top left corners
         let coord = getCoordinates(outputImage, i);
         let x = coord.x;
@@ -49,7 +50,7 @@ function rotate(inputImage, angle, interpolType){
         let pixelvalues = new Object();
         pixelvalues.alpha = 255;
 
-        if((originalCoord.x % 1)===0 && (originalCoord.y % 1)===0){
+        if((originalCoord.x % 1) === 0 && (originalCoord.y % 1) === 0){
             let index = getIndex(inputImage, x, y);
             pixelvalues.red = inputImage.data[index + 0];
             pixelvalues.green = inputImage.data[index + 1];
@@ -66,4 +67,61 @@ function rotate(inputImage, angle, interpolType){
     }
 
     return outputImage;
+}
+
+/**
+ * Returns the image scaled by a factor of 2 ^ scaleFactor
+ * @param {ImageData} inputImage
+ * @param {boolean} toDownScale
+ * @param {number} scaleFactor
+ * @param {string} interpolType
+ */
+function scale(inputImage, toUpScale, scaleFactor, interpolType) {
+    let outputImage = new ImageData(Math.round(inputImage.width * Math.pow(2, toUpScale ? scaleFactor : (-1 * scaleFactor))), Math.round(inputImage.height * Math.pow(2, toUpScale ? scaleFactor : (-1 * scaleFactor))));
+
+    let origCoord = new Object();
+    let currCoord = new Object();
+
+    if(toUpScale) {
+        for(let i = 0; i <= outputImage.data.length; i += 4) {
+    
+            currCoord = getCoordinates(outputImage, i);
+            origCoord = {
+                x : currCoord.x / Math.pow(2, scaleFactor),
+                y : currCoord.y / Math.pow(2, scaleFactor)
+            }
+            if(origCoord.x % 1 === 0 && origCoord.y % 1 === 0) {
+                let tempIndex = getIndex(inputImage, origCoord.x, origCoord.y);
+                outputImage[i + 0] = inputImage[tempIndex + 0];
+                outputImage[i + 1] = inputImage[tempIndex + 1];
+                outputImage[i + 2] = inputImage[tempIndex + 2];
+                outputImage[i + 3] = 255;
+            }
+            else {
+                let pixelvalues = interpolate(inputImage, currCoord.x, currCoord.y, interpolType);
+                outputImage[i + 0] = pixelvalues.red;
+                outputImage[i + 1] = pixelvalues.green;
+                outputImage[i + 2] = pixelvalues.blue;
+                outputImage[i + 3] = 255;
+            }
+        }
+    } else {
+        for (let i = 0; i <= outputImage.data.length; i += 4) {
+            currCoord = getCoordinates(outputImage, i);
+            origCoord = {
+                x : currCoord.x * Math.pow(2, scaleFactor),
+                y : currCoord.y * Math.pow(2, scaleFactor)
+            }
+            if(origCoord.x % 1 === 0 && origCoord.y % 1 === 0 && origCoord.x < inputImage.width && origCoord.y < inputImage.height) {
+                tempIndex = getIndex(inputImage, origCoord.x, origCoord.y);
+                outputImage[i + 0] = inputImage[tempIndex + 0];
+                outputImage[i + 1] = inputImage[tempIndex + 1];
+                outputImage[i + 2] = inputImage[tempIndex + 2];
+                outputImage[i + 3] = 255;
+            }
+        }
+    }
+
+    return outputImage;
+
 }
