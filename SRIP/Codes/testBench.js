d2a8fd9b94fd82afb32de066cc844c942bf87d7a
Rohan Nawathe <rohannawathe@hotmail.com>
2019-08-06 19:31:01 +0530

    Updated page layout and design

--- SRIP/Codes/testBench.js
+++ SRIP/Codes/testBench.js
@@ -7,9 +7,22 @@ mosaicImg.src = "res/Mosaic.png"; // Set source path
 // }, false);
 
 let inputImCanvas = document.getElementById("inputImCanvas");
-mosaicImg.onload = function () {
-    let inputImCanvasCtx = inputImCanvas.getContext("2d");
-    inputImCanvasCtx.drawImage(mosaicImg, 0, 0);
-    inputImCanvasCtx.drawImage(mosaicImg, 0, 0, inputImCanvas.width, inputImCanvas.height);
-};
+// mosaicImg.onload = function () {
+//     let inputImCanvasCtx = inputImCanvas.getContext("2d");
+//     inputImCanvasCtx.drawImage(mosaicImg, 0, 0);
+//     inputImCanvasCtx.drawImage(mosaicImg, 0, 0, inputImCanvas.width, inputImCanvas.height);
+// };
 
+function selectImage() {
+    $("#MosaicImgModal").modal('show');
+    // $("#MosaicImgModal").on('shown.bs.modal',function (e) {});
+    $("#mosaicSelectImage").imgAreaSelect({
+        handles: true,
+        autoHide: true,
+        // minHeight: 300,
+        // maxHeight: 300,
+        aspectRatio: "1:1",
+        resizeable: false,
+        onSelectEnd: function (img, selection) {}
+    });
+}
