e4823fe6c305f9955362f172b25f6a39ae259158
Rohan Nawathe <rohannawathe@hotmail.com>
2019-09-01 23:24:47 +0530

    Added function to change views

---
+++ SRIP/Codes/changeView.js
@@ -0,0 +1,39 @@
+function showStartView() {
+    $(".controlDiv").hide();
+    $("#startControls").show();
+}
+
+function showAffineView() {
+    $(".controlDiv").hide();
+    $("#affineControls").show();
+}
+
+function showPointView() {
+    $(".controlDiv").hide();
+    $("#pointControls").show();
+}
+
+function showMorphView() {
+    $(".controlDiv").hide();
+    $("#morphControls").show();
+}
+
+function showHistogramView() {
+    $(".controlDiv").hide();
+    $("#histogramControls").show();
+}
+
+function showNeighbourhoodView() {
+    $(".controlDiv").hide();
+    $("#neighbourhoodControls").show();
+}
+
+function showFourierView() {
+    $(".controlDiv").hide();
+    $("#fourierControls").show();
+}
+
+function showSegmentationView() {
+    $(".controlDiv").hide();
+    $("#segmentationControls").show();
+}
