40e304c47818302dea513e7e34f63d2fb1d82fab
WinstonWolf1 <rohannawathe@hotmail.com>
2019-12-27 01:38:19 +0530

    Moved from onclick to event listeners

--- SRIP/Codes/changeView.js
+++ SRIP/Codes/changeView.js
@@ -1,101 +1,152 @@
+document.getElementById("backButton").addEventListener("click", showStartView);
+
 function showStartView() {
-  $(".controlDiv").hide();
-  $("#startControls").show();
-  $("#backButton").hide();
-  $("#runButton").addClass("hide-back");
+    $(".controlDiv").hide();
+    $("#startControls").show();
+    $("#backButton").hide();
+    $("#runButton").addClass("hide-back");
 }
+
+document
+    .getElementById("affineRadio")
+    .addEventListener("click", showAffineView);
 
 function showAffineView() {
-  $(".controlDiv").hide();
-  $("#affineControls").show();
-  $("#backButton").show();
-  $("#runButton").removeClass("hide-back");
+    $(".controlDiv").hide();
+    $("#affineControls").show();
+    $("#backButton").show();
+    $("#runButton").removeClass("hide-back");
 }
+
+document.getElementById("pointRadio").addEventListener("click", function() {
+    showPointView("linear");
+});
+
+document
+    .getElementById("linearButtonPtCtrls")
+    .addEventListener("click", function() {
+        showPointView("linear");
+    });
+
+document
+    .getElementById("nonLinearButtonPtCtrls")
+    .addEventListener("click", function() {
+        showPointView("nonLinear");
+    });
+
+document
+    .getElementById("clippingButtonPtCtrls")
+    .addEventListener("click", function() {
+        showPointView("clipping");
+    });
+
+document
+    .getElementById("windowButtonPtCtrls")
+    .addEventListener("click", function() {
+        showPointView("window");
+    });
 
 function showPointView(type) {
-  $(".controlDiv").hide();
-  $("#pointControls").show();
-  $("#backButton").show();
-  $("#runButton").removeClass("hide-back");
-  switch (type) {
-    case "linear":
-      $("#linearControls").show();
-      $("#nonLinearControls").hide();
-      $("#clippingControls").hide();
-      $("#windowControls").hide();
-      $("#linearButtonPtCtrls").addClass("active");
-      $("#nonLinearButtonPtCtrls").removeClass("active");
-      $("#clippingButtonPtCtrls").removeClass("active");
-      $("#windowButtonPtCtrls").removeClass("active");
-      // $('#pointControls').
-      break;
-    case "nonLinear":
-      $("#linearControls").hide();
-      $("#nonLinearControls").show();
-      $("#clippingControls").hide();
-      $("#windowControls").hide();
-      $("#linearButtonPtCtrls").removeClass("active");
-      $("#nonLinearButtonPtCtrls").addClass("active");
-      $("#clippingButtonPtCtrls").removeClass("active");
-      $("#windowButtonPtCtrls").removeClass("active");
-      break;
-    case "clipping":
-      $("#linearControls").hide();
-      $("#nonLinearControls").hide();
-      $("#clippingControls").show();
-      $("#windowControls").hide();
-      $("#linearButtonPtCtrls").removeClass("active");
-      $("#nonLinearButtonPtCtrls").removeClass("active");
-      $("#clippingButtonPtCtrls").addClass("active");
-      $("#windowButtonPtCtrls").removeClass("active");
-      break;
-    case "window":
-      $("#linearControls").hide();
-      $("#nonLinearControls").hide();
-      $("#clippingControls").hide();
-      $("#windowControls").show();
-      $("#linearButtonPtCtrls").removeClass("active");
-      $("#nonLinearButtonPtCtrls").removeClass("active");
-      $("#clippingButtonPtCtrls").removeClass("active");
-      $("#windowButtonPtCtrls").addClass("active");
-      break;
+    $(".controlDiv").hide();
+    $("#pointControls").show();
+    $("#backButton").show();
+    $("#runButton").removeClass("hide-back");
+    switch (type) {
+        case "nonLinear":
+            $("#linearControls").hide();
+            $("#nonLinearControls").show();
+            $("#clippingControls").hide();
+            $("#windowControls").hide();
+            $("#linearButtonPtCtrls").removeClass("active");
+            $("#nonLinearButtonPtCtrls").addClass("active");
+            $("#clippingButtonPtCtrls").removeClass("active");
+            $("#windowButtonPtCtrls").removeClass("active");
+            break;
+        case "clipping":
+            $("#linearControls").hide();
+            $("#nonLinearControls").hide();
+            $("#clippingControls").show();
+            $("#windowControls").hide();
+            $("#linearButtonPtCtrls").removeClass("active");
+            $("#nonLinearButtonPtCtrls").removeClass("active");
+            $("#clippingButtonPtCtrls").addClass("active");
+            $("#windowButtonPtCtrls").removeClass("active");
+            break;
+        case "window":
+            $("#linearControls").hide();
+            $("#nonLinearControls").hide();
+            $("#clippingControls").hide();
+            $("#windowControls").show();
+            $("#linearButtonPtCtrls").removeClass("active");
+            $("#nonLinearButtonPtCtrls").removeClass("active");
+            $("#clippingButtonPtCtrls").removeClass("active");
+            $("#windowButtonPtCtrls").addClass("active");
+            break;
 
-    default:
-      break;
-  }
+        case "linear":
+        default:
+            $("#linearControls").show();
+            $("#nonLinearControls").hide();
+            $("#clippingControls").hide();
+            $("#windowControls").hide();
+            $("#linearButtonPtCtrls").addClass("active");
+            $("#nonLinearButtonPtCtrls").removeClass("active");
+            $("#clippingButtonPtCtrls").removeClass("active");
+            $("#windowButtonPtCtrls").removeClass("active");
+            // $('#pointControls').
+            break;
+    }
 }
+
+document.getElementById("morphRadio").addEventListener("click", showMorphView);
 
 function showMorphView() {
-  $(".controlDiv").hide();
-  $("#morphControls").show();
-  $("#backButton").show();
-  $("#runButton").removeClass("hide-back");
+    $(".controlDiv").hide();
+    $("#morphControls").show();
+    $("#backButton").show();
+    $("#runButton").removeClass("hide-back");
 }
+
+document
+    .getElementById("histogramRadio")
+    .addEventListener("click", showHistogramView);
 
 function showHistogramView() {
-  $(".controlDiv").hide();
-  $("#histogramControls").show();
-  $("#backButton").show();
-  $("#runButton").removeClass("hide-back");
+    $(".controlDiv").hide();
+    $("#histogramControls").show();
+    $("#backButton").show();
+    $("#runButton").removeClass("hide-back");
 }
+
+document
+    .getElementById("neighbourRadio")
+    .addEventListener("click", showNeighbourhoodView);
 
 function showNeighbourhoodView() {
-  $(".controlDiv").hide();
-  $("#neighbourhoodControls").show();
-  $("#backButton").show();
-  $("#runButton").removeClass("hide-back");
+    $(".controlDiv").hide();
+    $("#neighbourhoodControls").show();
+    $("#backButton").show();
+    $("#runButton").removeClass("hide-back");
 }
+
+document
+    .getElementById("fourierRadio")
+    .addEventListener("click", showFourierView);
 
 function showFourierView() {
-  $(".controlDiv").hide();
-  $("#fourierControls").show();
-  $("#backButton").show();
-  $("#runButton").removeClass("hide-back");
+    $(".controlDiv").hide();
+    $("#fourierControls").show();
+    $("#backButton").show();
+    $("#runButton").removeClass("hide-back");
 }
 
+document
+    .getElementById("segmentRadio")
+    .addEventListener("click", showSegmentationView);
+
 function showSegmentationView() {
-  $(".controlDiv").hide();
-  $("#segmentationControls").show();
-  $("#backButton").show();
-  $("#runButton").removeClass("hide-back");
+    $(".controlDiv").hide();
+    $("#segmentationControls").show();
+    $("#backButton").show();
+    $("#runButton").removeClass("hide-back");
 }
--- SRIP/Codes/testBench.js
+++ SRIP/Codes/testBench.js
@@ -12,50 +12,56 @@ mosaicImg.src = "res/Mosaic.png"; // Set source path
 //     inputImCanvasCtx.drawImage(mosaicImg, 0, 0, inputImCanvas.width, inputImCanvas.height);
 // };
 
+document
+  .getElementById("selectImgButton")
+  .addEventListener("click", selectImage);
+
 function selectImage() {
-    "use strict";
-    var inputImCanvas = document.getElementById("inputImCanvas"),
-        inputImCanvasCtx = inputImCanvas.getContext("2d");
-    $("#MosaicImgModal").modal("show");
-    // $("#MosaicImgModal").on('shown.bs.modal',function (e) {});
-    $("#mosaicSelectImage").imgAreaSelect({
-        handles: true,
-        autoHide: true,
-        minHeight: 100,
-        // maxHeight: 300,
-        aspectRatio: "1:1",
-        resizeable: false,
-        onSelectEnd: function(img, selection) {
-            // As the mosaic is scaled when shown in the modal, using scaleFactor to scale it back
-            // let scaleFactor = 900 / (0.76 * $(window).height());
-            let scaleFactor = 900 / 700;
-            inputImCanvasCtx.drawImage(
-                img,
-                Math.round(selection.x1 * scaleFactor),
-                Math.round(selection.y1 * scaleFactor),
-                Math.round(selection.width * scaleFactor),
-                Math.round(selection.height * scaleFactor),
-                0,
-                0,
-                inputImCanvas.width,
-                inputImCanvas.height
-            );
-            $("#selectImgButton").addClass("disabled");
-            $("#selectImgButton").attr("disabled", true);
-            $("#resetButton").removeClass("disabled");
-            $("#resetButton").attr("disabled", false);
-            $("#MosaicImgModal").modal("hide");
-        },
-    });
+  "use strict";
+  var inputImCanvas = document.getElementById("inputImCanvas"),
+    inputImCanvasCtx = inputImCanvas.getContext("2d");
+  $("#MosaicImgModal").modal("show");
+  // $("#MosaicImgModal").on('shown.bs.modal',function (e) {});
+  $("#mosaicSelectImage").imgAreaSelect({
+    handles: true,
+    autoHide: true,
+    minHeight: 100,
+    // maxHeight: 300,
+    aspectRatio: "1:1",
+    resizeable: false,
+    onSelectEnd: function(img, selection) {
+      // As the mosaic is scaled when shown in the modal, using scaleFactor to scale it back
+      // let scaleFactor = 900 / (0.76 * $(window).height());
+      let scaleFactor = 900 / 700;
+      inputImCanvasCtx.drawImage(
+        img,
+        Math.round(selection.x1 * scaleFactor),
+        Math.round(selection.y1 * scaleFactor),
+        Math.round(selection.width * scaleFactor),
+        Math.round(selection.height * scaleFactor),
+        0,
+        0,
+        inputImCanvas.width,
+        inputImCanvas.height
+      );
+      $("#selectImgButton").addClass("disabled");
+      $("#selectImgButton").attr("disabled", true);
+      $("#resetButton").removeClass("disabled");
+      $("#resetButton").attr("disabled", false);
+      $("#MosaicImgModal").modal("hide");
+    }
+  });
 }
 
+document.getElementById("resetButton").addEventListener("click", reset);
+
 function reset() {
-    "use strict";
-    var inputImCanvas = document.getElementById("inputImCanvas"),
-        inputImCanvasCtx = inputImCanvas.getContext("2d");
-    inputImCanvasCtx.clearRect(0, 0, inputImCanvas.width, inputImCanvas.height);
-    $("#selectImgButton").removeClass("disabled");
-    $("#selectImgButton").attr("disabled", false);
-    $("#resetButton").addClass("disabled");
-    $("#resetButton").attr("disabled", true);
+  "use strict";
+  var inputImCanvas = document.getElementById("inputImCanvas"),
+    inputImCanvasCtx = inputImCanvas.getContext("2d");
+  inputImCanvasCtx.clearRect(0, 0, inputImCanvas.width, inputImCanvas.height);
+  $("#selectImgButton").removeClass("disabled");
+  $("#selectImgButton").attr("disabled", false);
+  $("#resetButton").addClass("disabled");
+  $("#resetButton").attr("disabled", true);
 }
